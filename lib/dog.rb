0ef28b31b01a7feb91f5db4df40b308818bec2d0
Jeffrey Martinez <martinezjf2@gmail.com>
2019-12-22 19:35:08 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,5 +1,9 @@
 class Dog
   def initialize
+
 end
 end
 
+fido = Dog.new
+snoopy = Dog.new
+lassie = Dog.new
--- lib/person.rb
+++ lib/person.rb
@@ -2,3 +2,6 @@ class Person
   def initialize
 end
 end
+
+adele_goldberg = Person.new
+alan_kay = Person.new
